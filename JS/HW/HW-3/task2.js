a63d3c74031daef001fcc1dc4226950251556126
Kateryna <ekaterinamkg@gmail.com>
2025-12-17 14:54:16 +0100

    Add HW3

---
+++ JS/HW/HW-3/task2.js
@@ -0,0 +1,31 @@
+let averageGrade = 91;
+switch (true) {
+    case averageGrade <60:
+        console.log("Незадовільно");
+        break;
+    case averageGrade <=70:
+        console.log("Задовільно");
+        break;
+    case averageGrade <=80:
+        console.log("Добре");
+        break;
+    case averageGrade <=90:
+        console.log("Дуже добре");
+        break;
+    case averageGrade <=100:
+        console.log("Відмінно");
+        break;
+}
+
+    
+//     {
+//   console.log("Незадовільно");
+// } else if (averageGrade <= 70) {
+//   console.log("Задовільно");
+// } else if (averageGrade <= 80) {
+//   console.log("Добре");
+// } else if (averageGrade <= 90) {
+//   console.log("Дуже добре");
+// } else if (averageGrade <= 100) {
+//   console.log("Відмінно");
+// }
