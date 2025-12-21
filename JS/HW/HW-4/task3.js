304561814cab40ade14e1618199a608a80d84302
Kateryna <ekaterinamkg@gmail.com>
2025-12-21 17:36:05 +0100

    HW-4 function

---
+++ JS/HW/HW-4/task3.js
@@ -0,0 +1,15 @@
+function checkOrder(available, ordered) {
+  if (available < ordered) {
+    return `Your order is too large, we don’t have enough goods.`;
+  }
+  if(ordered == 0){
+    return `Your order is empty`;
+  }
+  else{
+    return `Your order is accepted` ;
+  }
+}
+
+console.log(checkOrder(15, 10));
+console.log(checkOrder(15, 0));
+console.log(checkOrder(8, 10))
