198b5ff189e1dad890dbec5a5880711112e0a58b
Kateryna <ekaterinamkg@gmail.com>
2026-01-04 19:47:05 +0100

    Add HW-7: Objects

---
+++ JS/HW/HW-7/task3.js
@@ -0,0 +1,15 @@
+let car1 = {
+  brand: "Renault",
+  model: "Megan",
+  year: "2019",
+};
+
+let car2 = {
+  brand: "Volvo",
+  model: "S80",
+  owner: "car owner",
+};
+
+let car3 = { ...car1, ...car2 };
+
+console.log(car3);
